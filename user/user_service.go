7ffabc749b668426c42cfefb63b7b7759cf21674
林海鹏 <haipeng_lin@163.com>
2025-04-19 23:16:24 +0800

    feat：【注册】接口

--- user/user_service.go
+++ user/user_service.go
@@ -13,7 +13,7 @@ import (
 func GetProfile(c *gin.Context) {
 	username := c.Param("username")
 	// 查询 User
-	userModel, err := SelectUser(&UserModel{Username: username})
+	userModel, err := SelectUser(&User{Username: username})
 	if err != nil {
 		c.JSON(http.StatusNotFound, common.NewError("profile", errors.New("请检查")))
 		return
@@ -30,11 +30,6 @@ func GetProfile(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"profile": profile})
 }
 
-// 用户注册
-func UsersRegist(c *gin.Context) {
-
-}
-
 // 用户登录、认证
 func UsersLogin(c *gin.Context) {
 
@@ -43,17 +38,14 @@ func UsersLogin(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, common.NewError("登录", errors.New("请确认数据填写是否规范！")))
 		return
 	}
-
 	// 访问 User 字段
 	user := LoginUserDTO.User
-
 	// 根据邮箱和密码查询，判断用户是否存在
-	userModel, err := SelectUser(&UserModel{Email: user.Email, Password: user.Password})
+	userModel, err := SelectUser(&User{Email: user.Email, Password: user.Password})
 	if err != nil {
-		c.JSON(http.StatusForbidden, common.NewError("登录", errors.New("邮箱或密码错误，请检查！")))
+		c.JSON(http.StatusForbidden, common.NewError("用户登录", errors.New("邮箱或密码错误，请检查！")))
 		return
 	}
-
 	// 转换为VO
 	userVO := UserVO{
 		Username: userModel.Username,
@@ -64,6 +56,32 @@ func UsersLogin(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"user": userVO})
+}
+
+// 用户注册
+func UsersRegist(c *gin.Context) {
+
+	// 校验数据
+	if err := c.ShouldBindJSON(&RegistUserDTO); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, common.NewError("用户注册", errors.New("请确认数据填写是否规范！")))
+		return
+	}
+	// 取到 User
+	var user = RegistUserDTO.User
+	// 保存用户
+	if err := SaveUser(&user); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err)
+		return
+	}
+	// TODO：还需要取到完整数据
+	userVO := UserVO{
+		Username: user.Username,
+		Email:    user.Email,
+		Bio:      "",
+		Image:    "",
+		Token:    "",
+	}
+	c.JSON(http.StatusCreated, gin.H{"user": userVO})
 }
 
 // 关注用户
